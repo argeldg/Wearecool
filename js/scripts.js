9815ddd814db1e31494a19d9c34da3bee325de2f
Argel <argeldegante98@gmail.com>
2024-01-19 14:15:45 -0600

    Banner de inicio y estructura de portafolio en construccion

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+
+//movimeinto ramdom//
+function moveImageRandomly(element) {
+    const maxX = window.innerWidth - element.width;
+    const maxY = window.innerHeight - element.height;
+
+    const newLeft = Math.random() * maxX;
+    const newTop = Math.random() * maxY;
+
+    element.style.left = `${newLeft}px`;
+    element.style.top = `${newTop}px`;
+}
+
+function mouseListener(elementName) {
+    const element = document.getElementById(elementName);
+
+    element.addEventListener('mouseover', function (event) {
+        moveImageRandomly(element);
+    });
+    element.addEventListener('mousemove', function (event) {
+        moveImageRandomly(element);
+    });
+}
+
+mouseListener('bubble1')
+mouseListener('bubble2')
+
+ //Hello//
+const hello_options = ["Hello", "Hola", "こんにちは", "Привет", "你好", "Bonjour", "مرحبا"];
+const textElement = document.getElementById('displayText');
+let index = 0;
+
+setInterval(function () {
+    textElement.classList.add('animate__fadeIn')
+    textElement.textContent = hello_options[index];
+    index = (index + 1) % hello_options.length;
+    setTimeout(function () {
+        textElement.classList.remove('animate__fadeIn');
+    }, 900);
+}, 3000)
+
+
